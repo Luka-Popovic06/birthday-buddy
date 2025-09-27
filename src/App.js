4ce8cf79d8f11d7fc1073d83cff47892ffd3a7e9
Luka Popovic <lukapopovic0111@gmail.com>
2025-09-27 16:31:59 +0200

    add initial birthday data array

--- src/App.js
+++ src/App.js
@@ -1,3 +1,40 @@
 import "./App.css";
 import React, { useState } from "react";
 import Birthdays from "./Birthdays";
+const boddy = [
+  {
+    img: "person-1.jpeg",
+    firstName: "Bertie",
+    lastName: "Yates",
+    years: 29,
+    id: 1,
+  },
+  {
+    img: "person-2.jpeg",
+    firstName: "Hester",
+    lastName: "Hogan",
+    years: 32,
+    id: 2,
+  },
+  {
+    img: "person-3.jpeg",
+    firstName: "Larry",
+    lastName: "Little",
+    years: 36,
+    id: 3,
+  },
+  {
+    img: "person-4.jpeg",
+    firstName: "Sean",
+    lastName: "Walsh",
+    years: 34,
+    id: 4,
+  },
+  {
+    img: "person-5.jpeg",
+    firstName: "Lola",
+    lastName: "Gardner",
+    years: 29,
+    id: 5,
+  },
+];
